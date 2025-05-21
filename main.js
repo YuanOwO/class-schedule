279f2c10a3c0f54d65546d3da4d79a9fbfa91e88
YuanOwO <yuan@yuanowo.xyz>
2025-05-21 14:09:32 +0800

    新學期資料

--- main.js
+++ main.js
@@ -4,12 +4,17 @@ const CONFIG = {
     },
     SCHEDULE_DATA = {},
     get_data = (semester) => {
+        /**
+         * 取得該學期的課程資訊
+         */
         function reqListener() {
+            // 請求到資料後
             if (this.status !== 200) {
-                console.error("Failed to load", file, "\nStatus Code:", this.status);
+                console.error(`Failed to load ${file}!\nStatus Code: ${this.status}`);
 
                 // render(semester);
 
+                // 清空表格
                 const tbody = document.getElementsByTagName("tbody")[0];
                 tbody.innerHTML = "";
 
@@ -37,6 +42,10 @@ const CONFIG = {
         oReq.send();
     },
     generate_schedule = (semester) => {
+        /**
+         * 將課程資訊轉換成課表
+         */
+
         let schedule = {},
             classdata = SCHEDULE_DATA[semester];
 
@@ -45,6 +54,7 @@ const CONFIG = {
         // console.log(semester);
         // console.log(classdata);
 
+        // 重設每周的天數
         CONFIG.wds = 5;
 
         for (const pkno in classdata) {
@@ -52,18 +62,28 @@ const CONFIG = {
                 const cls = classdata[pkno];
                 // console.log(cls);
                 for (let i = 0; i < cls.time.length; i++) {
+                    // 處理該節次
                     const t = cls.time[i] % 100,
                         w = (cls.time[i] - t) / 100;
+
                     if (w > CONFIG.wds) CONFIG.wds = w;
-                    schedule[cls.time[i]] = structuredClone(cls);
-                    schedule[cls.time[i]]["time"] = cls["time"][i];
-                    schedule[cls.time[i]]["classroom"] = cls["classroom"][i];
+
+                    let clscpy = structuredClone(cls);
+
+                    clscpy["time"] = cls["time"][i];
+                    clscpy["classroom"] = cls["classroom"][i];
+
+                    if (schedule[cls.time[i]] === undefined) schedule[cls.time[i]] = [];
+                    schedule[cls.time[i]].push(clscpy);
                 }
             }
         }
         return schedule;
     },
     render = (semester) => {
+        /**
+         * 顯示課表
+         */
         // 初始化
         const SCHEDULE = generate_schedule(semester),
             thead = document.getElementsByTagName("thead")[0],
@@ -119,35 +139,48 @@ const CONFIG = {
                 td = tr.appendChild(document.createElement("td"));
                 const t = ((j + 1) * 100 + i).toString(),
                     cls = SCHEDULE[t];
-                if (cls === undefined) td.innerText = "";
-                else {
-                    td.onclick = () => show_info(semester, cls.pkno);
-                    // td.setAttribute("data-bs-toggle", "modal");
-                    // td.setAttribute("data-bs-target", "#infoModal");
+                if (cls === undefined) {
+                    // 空堂
+                    td.innerText = "";
+                } else {
+                    // 顯示該時間的課程
+                    for (let k = 0; k < cls.length; k++) {
+                        let clsdiv = document.createElement("div"),
+                            course = cls[k];
 
-                    // 課程名稱
-                    div = td.appendChild(document.createElement("div"));
-                    div.innerText = SCHEDULE[t]["name"]
-                        .replace("_", "\n")
-                        .replace("【", "\n【")
-                        .replace("）（", "）\n（");
-                    div.classList.add("fw-semibold");
+                        if (k > 0) td.appendChild(document.createElement("hr")); // 分隔線
 
-                    // 教師名稱
-                    div.setAttribute("data-bs-toggle", "tooltip");
-                    div.setAttribute("data-bs-placement", "top");
-                    div.setAttribute("data-bs-title", "教師：" + SCHEDULE[t]["lecturer"]);
+                        td.appendChild(clsdiv);
+                        clsdiv.classList.add("course");
+                        clsdiv.onclick = () => show_info(semester, course.pkno);
 
-                    // div = td.appendChild(document.createElement("div"));
-                    // div.innerText = SCHEDULE[t]['lecturer'];
+                        // clsdiv.setAttribute("data-bs-toggle", "modal");
+                        // clsdiv.setAttribute("data-bs-target", "#infoModal");
 
-                    // 教室名稱
-                    div = td.appendChild(document.createElement("div"));
-                    div.innerText = SCHEDULE[t]["classroom"];
-                    // div.classList.add("fw-light");
-                    div.setAttribute("data-bs-toggle", "tooltip");
-                    div.setAttribute("data-bs-placement", "bottom");
-                    div.setAttribute("data-bs-title", BUILDINGS[SCHEDULE[t]["classroom"].slice(0, 3)]);
+                        // 課程名稱
+                        div = clsdiv.appendChild(document.createElement("div"));
+                        div.innerText = course["name"]
+                            .replace("_", "\n")
+                            .replace("【", "\n【")
+                            .replace("）（", "）\n（");
+                        div.classList.add("fw-semibold");
+
+                        // 教師名稱
+                        div.setAttribute("data-bs-toggle", "tooltip");
+                        div.setAttribute("data-bs-placement", "top");
+                        div.setAttribute("data-bs-title", "教師：" + course["lecturer"]);
+
+                        // div = clsdiv.appendChild(document.createElement("div"));
+                        // div.innerText = course['lecturer'];
+
+                        // 教室名稱
+                        div = clsdiv.appendChild(document.createElement("div"));
+                        div.innerText = course["classroom"];
+                        // div.classList.add("fw-light");
+                        div.setAttribute("data-bs-toggle", "tooltip");
+                        div.setAttribute("data-bs-placement", "bottom");
+                        div.setAttribute("data-bs-title", BUILDINGS[course["classroom"].slice(0, 3)]);
+                    }
                 }
             }
         }
@@ -160,6 +193,7 @@ const CONFIG = {
     info_fileds = ["code", "name", "department", "lecturer", "grade", "credit", "time", "classroom", "MUST", "COSTERM"],
     outline_fileds = ["objective", "outline", "teaching_method", "syllabus", "evaluation"],
     show_info = (semester, code) => {
+        console.log("show_info", semester, code);
         const cls = SCHEDULE_DATA[semester][code],
             modal = new bootstrap.Modal("#infoModal"),
             container = document.getElementById("modal-container");
